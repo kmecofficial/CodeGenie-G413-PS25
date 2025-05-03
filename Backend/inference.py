92b9fc2a770f84490647a5e813ad0c3cfed0c916
Omkar M Shewalkar <omkar.shewalkar936@gmail.com>
2025-05-03 12:43:21 +0530

    minor changes

--- Backend/inference.py
+++ Backend/inference.py
@@ -10,7 +10,7 @@ def generate_code(context: str, language: str = "python"):
     prompt = f"<|system|>\nYou are a coding assistant. Only return valid {language} code with no explanations or comments.\n<|user|>\n{context}\n<|assistant|>"
 
     inputs = tokenizer(prompt, return_tensors='pt').to("cuda")
-    outputs = model.generate(**inputs, max_new_tokens=1024, temperature=0.3, top_p=0.9, do_sample=True)
+    outputs = model.generate(**inputs, max_new_tokens=1024, temperature=0.3, top_p=0.7, do_sample=True)
 
     result = tokenizer.decode(outputs[0], skip_special_tokens=True)
     generated = result.split("<|assistant|>")[-1].strip()
