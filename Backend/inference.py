1e55248179316691b84138bbea847974bc4923cf
Omkar M Shewalkar <omkar.shewalkar936@gmail.com>
2025-07-08 22:22:26 +0530

    Made changes in the prompt

--- Backend/inference.py
+++ Backend/inference.py
@@ -22,9 +22,9 @@ def generate_code(context: str, language: str = "python") -> str:
 
     try:
         prompt = (
-            f"<|system|>\nYou are a coding assistant. "
-            f"Only return valid {language} code with no explanations or comments.\n"
-            f"<|user|>\n{context}\n<|assistant|>"
+        f"Generate valid and complete {language} code based on the following request.\n"
+        f"Request:\n{context}\n"
+        f"Do not include any explanations or comments. Only return the output code.\n\n"
         )
 
         inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
